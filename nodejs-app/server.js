cf6c92f932e72f87da9b7ca4e8b7333efff255fd
Student User <student@workstation.lab.example.com>
2021-08-17 03:35:41 -0400

    debug

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -4,11 +4,12 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-    console.log('PTirumala app listening on port 8080!');
+  console.log('PTirumala app listening on port 8080!');
+  console.log('PT HOSTNAME: ' + process.env.HOSTNAME + '\n');
   res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('PT Example app listening on port 8080!');
 });
 
